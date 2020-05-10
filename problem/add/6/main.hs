99e1400a2b4227211449c5c4c247e52aac0eae31
Rudy Matela <rudy@matela.com.br>
2020-05-09 23:16:30 -0300

    Document more files

--- problem/add/6/main.hs
+++ problem/add/6/main.hs
@@ -1,3 +1,24 @@
+-- main.c: main file for an "add" solution
+--
+-- This imports the add function from submission file as a Main module
+--
+-- It processes standard input then the "in.txt" file.
+--
+--
+-- Copyright (C) 2020  Rudy Matela
+--
+-- This program is free software; you can redistribute it and/or
+-- modify it under the terms of the GNU General Public License
+-- as published by the Free Software Foundation; either version 2
+-- of the License, or (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see <http://www.gnu.org/licenses/>.
 module AltMain (main) where
 
 import Main (add)
