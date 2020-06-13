4f80ce180d2907ec72553265b594820acbfbe90b
Rudy Matela <rudy@matela.com.br>
2020-06-13 14:21:13 -0300

    Add add java example

---
+++ examples/add/add.java
@@ -0,0 +1,36 @@
+// examples/add/add.java: example correct solution to "add"
+//
+// This is a correct solution to the "add" problem.
+// It should get a full 6/6 score.
+//
+//
+// This file is part of Udge.
+//
+//
+// Copyright (C) 2020  Rudy Matela
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import java.util.Scanner;
+
+public class Add {
+    public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		while (in.hasNext()) {
+			int x = in.nextInt();
+			int y = in.nextInt();
+			System.out.println(x + y);
+		}
+    }
+}
