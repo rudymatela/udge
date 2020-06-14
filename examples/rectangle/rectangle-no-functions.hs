9ef5f00e364c84a36db22f124caa8299de843162
Rudy Matela <rudy@matela.com.br>
2020-06-13 21:26:32 -0300

    rectangle: add test set #3

---
+++ examples/rectangle/rectangle-no-functions.hs
@@ -0,0 +1,48 @@
+-- examples/add/add.hs: solution to the "rectangle" example problem
+--
+-- This program is an example solution to the "rectangle" example problem
+-- that gets a 2/3 score.
+--
+-- This file is part of Udge.
+--
+--
+-- Copyright (C) 2020  Rudy Matela
+--
+-- This program is free software; you can redistribute it and/or
+-- modify it under the terms of the GNU General Public License
+-- as published by the Free Software Foundation; either version 2
+-- of the License, or (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+data Rectangle  =  Rectangle { unit :: String
+                             , width :: Int
+                             , height :: Int
+                             }
+
+ar :: Rectangle -> Int
+ar (Rectangle _ w h)  =  w * h
+
+pe :: Rectangle -> Int
+pe (Rectangle _ w h)  =  2 * (w + h)
+
+main :: IO ()
+main  =  interact solve
+
+solve :: String -> String
+solve  =  unlines . map (solve1 . readRectangle) . lines
+
+readRectangle :: String -> Rectangle
+readRectangle s  =  Rectangle u (read w') (read h')  where  [w',h',u] = words s
+
+solve1 :: Rectangle -> String
+solve1 rectangle  =  "The area is " ++ show (ar rectangle)
+                  ++ " square " ++ unit rectangle
+                  ++ " and the perimeter is " ++ show (pe rectangle)
+                  ++ " " ++ unit rectangle ++ "."
---
+++ problem/rectangle/3/main.hs
@@ -0,0 +1,45 @@
+-- main.c: main file for an "rectangle" solution
+--
+-- This imports the add function from submission file as a Main module
+--
+-- It processes standard input then the "in.txt" file.
+--
+--
+-- Copyright (C) 2020  Rudy Matela
+--
+-- This program is free software; you can redistribute it and/or
+-- modify it under the terms of the GNU General Public License
+-- as published by the Free Software Foundation; either version 2
+-- of the License, or (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+module AltMain (main) where
+
+import Main (Rectangle(..), area, perimeter)
+
+main :: IO ()
+main = do
+  -- processes standard input
+  interact solve
+
+  -- processes in.txt
+  more <- readFile "in.txt"
+  putStr $ solve more
+
+solve :: String -> String
+solve  =  unlines . map (solve1 . readRectangle) . lines
+
+readRectangle :: String -> Rectangle
+readRectangle s  =  Rectangle u (read w') (read h')  where  [w',h',u] = words s
+
+solve1 :: Rectangle -> String
+solve1 rectangle@(Rectangle unit _ _)  =  "The area is " ++ show (area rectangle)
+                                       ++ " square " ++ unit
+                                       ++ " and the perimeter is " ++ show (perimeter rectangle)
+                                       ++ " " ++ unit ++ "."
