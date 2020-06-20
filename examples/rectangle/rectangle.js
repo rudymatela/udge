c7bf1a02ea6926fff9f25db69db0323086ff768e
Rudy Matela <rudy@matela.com.br>
2020-06-20 18:07:44 -0300

    Add JavaScript solution to rectangle

---
+++ examples/rectangle/rectangle.js
@@ -0,0 +1,44 @@
+// examples/rectangle/rectangle.js: example correct solution to "rectangle"
+//
+// This is a correct solution to the "rectangle" problem.
+// It should get a full 3/3 score.
+//
+//
+// This file is part of Udge.
+//
+//
+// Copyright (C) 2020  Rudy Matela
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+var rl = require('readline').createInterface({input: process.stdin, output: process.stdout});
+
+function area(rectangle) {
+  return rectangle.width * rectangle.height;
+}
+
+function perimeter(rectangle) {
+  return 2 * (rectangle.width + rectangle.height);
+}
+
+rl.on('line', function(line){
+  words = line.split(/ /);
+  rectangle = {
+    width:  parseInt(words[0]),
+    height: parseInt(words[1])
+  }
+  console.log(rectangle.width + "x" + rectangle.height + " rectangle, " +
+              "area = " + area(rectangle) + ", " +
+              "perimeter = " + perimeter(rectangle));
+})
---
+++ problem/rectangle/3/main.js
@@ -0,0 +1,36 @@
+// main.java: main file for an "rectangle" solution
+//
+// It processes values from standard input then from the "in.txt" file.
+// This is appended to the submitted JavaScript program and tests the add function.
+//
+// The submitted file processes the standard input and this processes the
+// "in.txt" file.
+//
+// Copyright (C) 2020  Rudy Matela
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+var fs = require('fs').createReadStream('in.txt');
+var rl = require('readline').createInterface({input: fs, output: process.stdout});
+
+rl.on('line', function(line){
+  words = line.split(/ /);
+  rectangle = {
+    width:  parseInt(words[0]),
+    height: parseInt(words[1])
+  }
+  console.log(rectangle.width + "x" + rectangle.height + " rectangle, " +
+              "area = " + area(rectangle) + ", " +
+              "perimeter = " + perimeter(rectangle));
+});
