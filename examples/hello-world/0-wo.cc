45a23d3163852932e5acf441995a5c52794ba4bb
Rudy Matela <rudy@matela.com.br>
2020-06-21 12:34:23 -0300

    Add C++ support (missing tests)

---
+++ examples/hello-world/0-wo.cc
@@ -0,0 +1,33 @@
+/*
+ * examples/hello-world/hello-world.cc: example incorrect solution to "hello-world"
+ *
+ * This is a correct solution to the "hello-world" problem.
+ * It should get a 0/1 score.
+ *
+ * This file is part of Udge.
+ *
+ *
+ * Copyright (C) 2020  Rudy Matela
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "eHllo, Wolrd!\n";
+	return 0;
+}
