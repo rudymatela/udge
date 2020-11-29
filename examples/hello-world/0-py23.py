464943e2611d319ad6c6149aa8387abac8254e6e
Rudy Matela <rudy@matela.com.br>
2020-11-28 21:31:07 -0300

    Test Python 2/3 switching

---
+++ examples/hello-world/0-py23.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+#
+# examples/hello-world/0-py23.py: example solution to the "hello-world" exercise
+#
+# This is an incorrect solution to the "hello-world" problem
+# that uses notation exclusive to Python 2 with a Python 3 shebang.
+#
+# This file is part of Udge.
+#
+#
+# Copyright (C) 2020  Rudy Matela
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+print "Hello, World!"
