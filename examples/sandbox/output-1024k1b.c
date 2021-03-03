888cc5ae2a7981e90f50648ec94721812ee4830c
Rudy Matela <rudy@matela.com.br>
2021-03-03 18:44:04 -0300

    add more output limit tests
    and fix output limit to be actually 1M

---
+++ examples/sandbox/output-1024k1b.c
@@ -0,0 +1,31 @@
+/*
+ * examples/sandbox/output-1024k1b.c: produces 1024KiB + 1B of output
+ *
+ *
+ * Copyright (C) 2020-2021  Rudy Matela
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+
+int main(void)
+{
+	int i;
+	for (i=0; i<1024*1024; i++)
+		putchar('\0');
+	putchar('\0');
+	return 0;
+}
